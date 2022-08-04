cc96c5c7af59377816b8873207f20095ddac4d80
Brahma behera <beherabrahmananda77@gmail.com>
2022-08-05 00:02:48 +0530

    homepage done

--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  sidebar :boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toogleSidebar(){
+    this.sidebar=!this.sidebar;
+  }
 }
